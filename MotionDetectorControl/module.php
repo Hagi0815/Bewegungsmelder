1df57a5e86f59b697d8089b59ba17d455533f5f6
Christian Hagedorn <hagis.iphone@googlemail.com>
2026-04-07 16:10:18 +0200

    Add files via upload

---
+++ MotionDetectorControl/module.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+class MotionDetectorControl extends IPSModule
+{
+    public function Create(): void
+    {
+        parent::Create();
+    }
+
+    public function ApplyChanges(): void
+    {
+        parent::ApplyChanges();
+    }
+}
